5807a973d3064387d026ab91e2e955c14c9bb1a8
iamskyline <1234fil1234@gmail.com>
2024-06-15 19:18:49 +0300

    Исправление окна формирования заявки от лица клиента

--- API/api.client/src/apps/rentalRequests/cards/rentalRequestClientForm.tsx
+++ API/api.client/src/apps/rentalRequests/cards/rentalRequestClientForm.tsx
@@ -13,29 +13,35 @@ export function RentalRequestClientForm(props: IProps) {
             onClose={props.onClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
-            maxWidth="sm"
+            maxWidth="lg"
             sx={{
-                borderRadius: 5
+                borderRadius: 5,
             }}
         >
-            <Typography variant="h5" align="center">
-                Выберите даты аренды автомобиля
-            </Typography>
-            <Grid item xs={12} md={6} lg={4}>
-                <DatePicker placeholder="Выберите дату начала аренды" />
-
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-                <DatePicker placeholder="Выберите дату окончания аренды" />
-
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-                <Button variant="outlined"
-                    onClick={props.onClose}
-                >
-                    Закрыть
-                </Button>
-            </Grid>
+            <Box padding={2}>
+                <Typography variant="h5" align="center" mb={2}>
+                    Выберите даты аренды автомобиля
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6} lg={6}>
+                        <DatePicker placeholder="Выберите дату начала аренды" />
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={6}>
+                        <DatePicker placeholder="Выберите дату окончания аренды" />
+                    </Grid>
+                    <Grid item xs={6} md={6} lg={6}>
+                        <Button variant="outlined"
+                            onClick={props.onClose}>
+                            Закрыть
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} md={6} lg={6} display="flex" justifyContent="flex-end">
+                        <Button variant="contained">
+                            Отправить заявку
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Box>
         </Dialog>
     )
 }
