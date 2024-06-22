296bce478894876d8c39ec5bfe3764f8749c60ce
Weazy <weazy@internet.ru>
2024-06-22 19:26:06 +0300

    Удаление ненужного каталога

--- API/api.client/vite.config.ts
+++ API/api.client/vite.config.ts
@@ -33,8 +33,6 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7220';
 
-console.log(target)
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
